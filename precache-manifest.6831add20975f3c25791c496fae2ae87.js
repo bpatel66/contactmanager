ad3c7830a403c1afc2d7b5b604a8e9ccec7cf5c3
Balkrishna Patel <balkrishna.h.patel@gmail.com>
2019-12-20 16:10:54 -0500

    Updates

---
+++ precache-manifest.6831add20975f3c25791c496fae2ae87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02cf32af63a712b9521e3631f2900be",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "6ad88a93f0f3e1217f0b",
+    "url": "/contactmanager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "825beabab279eaadb4c7",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "6ad88a93f0f3e1217f0b",
+    "url": "/contactmanager/static/js/2.fc9d69f2.chunk.js"
+  },
+  {
+    "revision": "c8336e0bcbf8ea1670aafb15aa044bf7",
+    "url": "/contactmanager/static/js/2.fc9d69f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "825beabab279eaadb4c7",
+    "url": "/contactmanager/static/js/main.15b74679.chunk.js"
+  },
+  {
+    "revision": "329d56e5d2a3841584cb",
+    "url": "/contactmanager/static/js/runtime-main.220e9dd0.js"
+  }
+]);
